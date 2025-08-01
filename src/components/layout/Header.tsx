fc29b5f9a14048561138640821b90f9b19efd0c3
WDK-Techno <wdilshankavindra@gmail.com>
2025-08-01 22:53:29 +0530

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -64,7 +64,7 @@ export const Header = ({ isMainPage }: HeaderProps) => {
                 className={`text-xs ${isScrolled ? "text-purple-600" : "text-white"
                   }`}
               >
-                IEEE Yong Proffessionals Sri Lanka
+                IEEE Young Professionals Sri Lanka
               </p>
             </div>
           </Link>
