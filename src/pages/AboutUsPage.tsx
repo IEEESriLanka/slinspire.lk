e055f8ca1b43813d21ab1ba4bef8349ae199cf37
WDK-Techno <wdilshankavindra@gmail.com>
2025-11-05 13:22:24 +0530

    Aboutus us Component Renaming

--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Header } from '../components/layout/Header'
 import { Footer } from '../components/layout/Footer'
-import { GoalsSection } from '../components/sections/AboutUsSection';
+import { AbousUsSection } from '../components/sections/AboutUsSection';
 
 export const AboutUsPage = () => {
 
@@ -9,7 +9,7 @@ export const AboutUsPage = () => {
         <div className="min-h-screen bg-gradient-to-br from-purple-50 to-indigo-50">
             <Header isMainPage={false} />
             <main className='max-w-7xl mx-auto px-4 py-24'>
-                <GoalsSection />
+                <AbousUsSection />
             </main>
             <Footer />
         </div>
