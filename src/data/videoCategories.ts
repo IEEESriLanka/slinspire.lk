c0840ba8e9046235b319d901fb3a5effb92d85ea
WDK-Techno <wdilshankavindra@gmail.com>
2025-11-05 20:47:31 +0530

    Video Category Descriptions updated

--- src/data/videoCategories.ts
+++ src/data/videoCategories.ts
@@ -9,15 +9,13 @@ export const videoCategories: VideoCategory[] = [
     {
         id: 4,
         name: "Career Compass - Career Guidance Programs",
-        description: "ඔබ දැනට සාමාන්‍ය පෙළ හෝ උසස් පෙළ හදාරන පාසල් ශිෂ්‍යයෙක් නම්, ඔබේ ඉදිරි විභාග සඳහා තෝරා ගත යුතු විෂයයන් හෝ විෂය ධාරාවන් පිළිබඳව ඔබ නොදන්නේ නම්, නැතහොත් ඔබ ශ්‍රී ලංකාවේ කුමන ආකාරයේ වෘත්තීය මාර්ග තිබේද යන්න පිළිබඳ මග පෙන්වීමක් අවශ්‍ය අයෙක් නම්, මෙම වෘත්තීය මාර්ගෝපදේශන සැසිය ඔබට උපකාර වනු ඇත.",
+        description: "An initiative aimed at Grade 10 and 11 students, providing insights into academic, vocational, and professional development opportunities.",
         image: "/categories/cc.png"
     },
     {
         id: 1,
         name: "නිවැරදි දිශානතිය උසස් පෙළින් අනාගතයට",
-        description: "මෙම වැඩසටහන් මාලාව හරහා, ඔබට උසස් පෙළ විෂය ක්ෂේත්‍රයන් පිළිබඳ විශ්වවිද්‍යාලීය කථිකාචාර්යවරුන් සමග පැවැත්වූ දේශන නැරඹීමට අවස්ථාව ලැබේ.  \n" +
-            "\n" +
-            "අධ්‍යාපනික දැනුම වර්ධනය කරගන්න, අලුත් අදහස් සොයාගන්න, සහ ඔබේ අනාගත අධ්‍යාපනික ගමන සදහා ප්‍රතිලාභ ලබා ගන්න. 🌟  ",
+        description: "A virtual program series for students after O/Ls, guiding them in selecting the right A/L stream.",
         image: "/categories/nd.png"
     },
     {
@@ -29,7 +27,7 @@ export const videoCategories: VideoCategory[] = [
     {
         id: 3,
         name: "Embedded Systems Workshop",
-        description: "Embedded systems are the future of computing! Get started in the embedded world, which is a combination of electronics and software engineering, by learning the basic building blocks. In this first workshop of the workshop series, you will get an overview of the domain, what the current and future trends look like, and learn about microcontrollers",
+        description: "This workshop explores the basics, current trends and microcontrollers to kickstart your journey into the future of computing.",
         image: "/categories/emb.png"
     },
 
